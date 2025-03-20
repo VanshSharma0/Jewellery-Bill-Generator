69909f94a63642df08d37e5316a924543cccc929
VanshSharma0 <vanshsharma9933@gmail.com>
2025-03-20 13:54:35 +0530

    add Firebase configuration and Firestore dependency

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBelUvkmgzay05xMvC0sNTRexsxMV0rTCU",
+    authDomain: "bill-gen-a4c7f.firebaseapp.com",
+    projectId: "bill-gen-a4c7f",
+    storageBucket: "bill-gen-a4c7f.firebasestorage.app",
+    messagingSenderId: "682162688340",
+    appId: "1:682162688340:web:b589e598ee827d556078c1",
+    measurementId: "G-7M2MCZEY26"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const db = getFirestore(app);
